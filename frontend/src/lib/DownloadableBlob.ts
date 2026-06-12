b6ed1e8f91dea95d6cef5aa9e1808d0349a2fd95
Thibault <thibault@faks.co>
2026-06-12 14:56:51 +0200

    chore: handle feedbacks

--- frontend/src/lib/DownloadableBlob.ts
+++ frontend/src/lib/DownloadableBlob.ts
@@ -53,22 +53,23 @@ export class DownloadableBlob extends Blob {
     filename?: string,
   ): DownloadableBlob {
     const binaryString = atob(data);
-    const bytes = new Uint8Array(binaryString.length);
-    for (let i = 0; i < binaryString.length; i++) {
-      bytes[i] = binaryString.charCodeAt(i);
-    }
+    const bytes = Uint8Array.from(binaryString, (c) => c.charCodeAt(0));
+
     return new DownloadableBlob([bytes], { type, filename });
   }
 
   download = () => {
     const url = URL.createObjectURL(this);
-    const link = document.createElement("a");
-    link.href = url;
-    link.download = this.filename;
-    link.style.display = "none"; // évite tout flash
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-    URL.revokeObjectURL(url);
+    try {
+      const link = document.createElement("a");
+      link.href = url;
+      link.download = this.filename;
+      link.style.display = "none"; // prevent flash
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    } finally {
+      URL.revokeObjectURL(url);
+    }
   };
 }
