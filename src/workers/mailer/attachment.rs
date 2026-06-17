fa35fa2ba8baef7b941d64f87d9f8c10730dfd21
Thibault <thibault@faks.co>
2026-06-17 14:21:05 +0200

    chore: handle feedbacks

--- src/workers/mailer/attachment.rs
+++ src/workers/mailer/attachment.rs
@@ -3,15 +3,15 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct EmailAttachment {
   pub filename: String,
-  pub content_type: &'static str,
+  pub content_type: String,
   pub data: String,
 }
 
 impl EmailAttachment {
-  pub fn from_bytes(filename: String, content_type: &'static str, data: &Vec<u8>) -> Self {
+  pub fn from_bytes(filename: String, content_type: &str, data: &Vec<u8>) -> Self {
     Self {
       filename,
-      content_type,
+      content_type: content_type.to_string(),
       data: base64::Engine::encode(&base64::engine::general_purpose::STANDARD, data),
     }
   }
